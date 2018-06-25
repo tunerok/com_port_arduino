a1118549873565bf5761f7ae3c6bda2841f197f4
Home Pc <tunerok93@gmail.com>
2018-06-25 23:58:37 +0300

    Дополнн код сортировкой

--- com_adc/com_adc.ino
+++ com_adc/com_adc.ino
@@ -1,12 +1,27 @@
+#DEFINE ITERS 4
+
+
 int sig_inp = 2; //Аналоговый вход с которого производится считываение
 int reset = 5; //Цифровой вывод, который дергает резет
 bool is_reading = false; //переменная для определения считывает ли контроллер информацию сейчас или нет
+int temp = 0; //переменная для считывания данных
+int val = 0;//переменная для хранения данных
+int iter = 0;//номер итерации, чтобы экрану не стало плохо
 
 void setup() {
   
-  Serial.begin(9600);//Скорость COM-порта 
-  pinMode(sig_inp, INPUT);//Установка аналогого  
+  Serial.begin(9600);//Скорость COM-порта 9600кб/с
+  pinMode(sig_inp, INPUT);//Установка аналогого 
+  pinMode(reset, OUTPUT);//установка резета  
 
+}
+
+void reset_and_show(){//функция сброса
+	show(val);
+	val = 0;
+	iter = 0;
+	digitalWrite(reset, HIGH);
+	
 }
 
 void loop() {
@@ -20,7 +35,13 @@ void loop() {
         default:{break;}
       }
     }
-
-    
-
+	
+	
+    temp = analogRead(analogPin);//чтение АЦП
+	if (temp > val)
+		val = temp;
+	if (iter == ITERS) //ждем ITERS итераций
+		reset_and_show();
+	iter++;
+	
 }
