6b551f419c2c96f522ed878778e61967bcbec230
aashirov <tunerok93@gmail.com>
2018-09-04 11:52:42 +0300

    Добавлена возможность работы платы без подключения к программе на ПК Добавлен вывод результата на LCD 16*2 дисплей

--- com_adc/com_adc.ino
+++ com_adc/com_adc.ino
@@ -1,3 +1,5 @@
+#include <LiquidCrystal.h>
+
 #define ITERS 4 //Используется для замедления выдачи значений в порт
 #define MY_ID "TfLjQmm3XZgiqdNA" // ID устройства по которому можно его найти через поиск по COM-портам
 #define KEY_LENGTH 16
@@ -14,6 +16,13 @@ char imput_id[KEY_LENGTH];
 bool answer = false; //Для проверки состояния ответа
 
 
+bool with_pc = false; // Для отладки платы без программы на пк
+
+// Инициализируем объект-экран, передаём использованные
+// для подключения контакты на Arduino в порядке:
+// RS, E, DB4, DB5, DB6, DB7
+LiquidCrystal lcd(4, 5, 10, 11, 12, 13);
+
 byte inc_byte;
 
 //bool chk_conn = false; //Бул для проверки состояния коннекта
@@ -21,11 +30,18 @@ byte inc_byte;
 //bool is_reading = false; //переменная для определения считывает ли контроллер информацию сейчас или нет
 
 void setup() {
-  i = 0;
-  while (i < KEY_LENGTH)
-  {
-    imput_id[i] = MY_ID[i];
-    i++;
+  // устанавливаем размер (количество столбцов и строк) экрана
+  lcd.begin(16, 2);
+  // очищаем дисплей
+  lcd.clear();
+
+  if (with_pc) {
+    i = 0;
+    while (i < KEY_LENGTH)
+    {
+      imput_id[i] = MY_ID[i];
+      i++;
+    }
   }
 
   Serial.begin(9600);//Скорость COM-порта 9600кб/с
@@ -34,12 +50,14 @@ void setup() {
 
 }
 
-void show(double doub){
-  
-  }
+void show() {
+  lcd.clear();
+  lcd.print(val);
+
+}
 
 void reset_and_show() { //Функция сброса
-  show(val);
+  show();
   val = 0;
   iter = 0;
   digitalWrite(reset, HIGH);
@@ -49,40 +67,38 @@ void reset_and_show() { //Функция сброса
 void loop() {
   i = 0;
   answer = false;
-  while ((Serial.available() > 0)) {
-    inc_byte = Serial.read();
-    if (imput_id[i] == inc_byte)
-      answer = true;
-    else if (inc_byte == "C"){
-		Serial.print("E")
-		
-	}
-	else
-      answer = false;
-    i++;
-    if ((i == KEY_LENGTH - 1) && (answer == true))
-      Serial.print("MY_ID");
-    chk_conn = true;
+  if (with_pc) {
+    while ((Serial.available() > 0)) {
+      inc_byte = Serial.read();
+      if (imput_id[i] == inc_byte)
+        answer = true;
+      else if (inc_byte == "C") {
+        Serial.print("E");
+
+      }
+      else
+        answer = false;
+      i++;
+      if ((i == KEY_LENGTH - 1) && (answer == true))
+        Serial.print("MY_ID");
+      //chk_conn = true;
+    }
+
   }
-
-
   // if ((chk_conn) && (counter == COUNTS)) { //проверка подключения эхо-запросом
-    // Serial.print(6);
-    // inc_byte = Serial.read();
-    // if (inc_byte == '7')
-      // chk_conn = true;
-    // else
-      // chk_conn = false;
+  // Serial.print(6);
+  // inc_byte = Serial.read();
+  // if (inc_byte == '7')
+  // chk_conn = true;
+  // else
+  // chk_conn = false;
   // }
-  
-    temp = analogRead(sig_inp);//чтение АЦП
-    if (temp > val)
-      val = temp;
-    if (iter == ITERS) //ждем ITERS итераций
-      reset_and_show();
-    iter++;
-    //counter++;
-  }
 
-
+  temp = analogRead(sig_inp);//чтение АЦП
+  if (temp > val)
+    val = temp;
+  if (iter == ITERS) //ждем ITERS итераций
+    reset_and_show();
+  iter++;
+  //counter++;
 }
